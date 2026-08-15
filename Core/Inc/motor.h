60081c98e148ed674cb03bbdf77cbe0bb9cc210a
Michael Yang <my8308second@gmail.com>
2026-08-14 20:18:59 -0700

    completed motor.c

--- Core/Inc/motor.h
+++ Core/Inc/motor.h
@@ -3,21 +3,9 @@
 
 #include "stm32f4xx.h"
 
-typedef struct {
-    TIM_HandleTypeDef *htim_encoder;
-    TIM_HandleTypeDef *htim_pwm;
-    uint32_t pwm_channel;
-    GPIO_TypeDef *dir_port;
-    uint16_t dir_pin1;
-    uint16_t dir_pin2;
-    int32_t target_pos;
-    float ramped_target, final_target, ramp_step;
-    float integral_pos, prev_pos_error;
-    float Kp, Ki, Kd;
-    float last_pwm_output;
-} MotorController;
 
-void print_encoder_count(MotorController *motor1 , UART_HandleTypeDef *huart);
+
+void print_encoder_count(void);
 void pos_pid(float dt);
 void motor_forward(uint8_t forward);
 void upated_ramp_target(void);
--- Core/Src/motor.c
+++ Core/Src/motor.c
@@ -4,13 +4,29 @@
 #include "stm32f4xx_hal_uart.h"
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h> 
 
+#define MAX_PWM 999
+#define COUNTS_PER_REV 464.0
 
+int32_t target_pos = 0;
+float pos_Kp = 14.92f;
+float pos_Ki = 50.0f;
+float pos_Kd = 0.2792f;
+float integral_pos = 0.0f;
+float prev_pos_error = 0.0f;
+float last_pwm_output = 0.0f;
+float ramped_target = 0.0f;
+float final_target = 0.0f;
+float ramp_step = 5.0f;
 
+extern TIM_HandleTypeDef htim1;
+extern TIM_HandleTypeDef htim2;
+extern UART_HandleTypeDef huart2;
 
-void print_encoder_count(MotorController *motor1 , UART_HandleTypeDef *huart)
+void print_encoder_count(void)
 {
-    int32_t counts = __HAL_TIM_GET_COUNTER(motor -> htim_encoder);
+    int32_t counts = __HAL_TIM_GET_COUNTER(&htim2);
     char msg[50];
     sprintf(msg, "%ld,%ld,%.1f\r\n",pos_target,counts,last_output_pwm);
     HAL_UART_Transmit(huart, (uint8_t *) msg, strlen(msg), 100);
@@ -36,12 +52,12 @@ void pos_pid(float dt)
     if(output >= 0)
     {
         motor_forward(1);
-        __HAL_TIM_SET_COMPARE(__HANDLE__, __CHANNEL__, __COMPARE__);
+        __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, (uint32_t)output);
     }
     else 
     {
         motor_forward(0);
-        __HAL_TIM_SET_COMPARE(__HANDLE__, __CHANNEL__, __COMPARE__);
+        __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, (uint32_t)(-output));
     }
 }
 
@@ -49,13 +65,13 @@ void motor_forward(uint8_t forward)
 {
     if(forward)
     {
-        HAL_GPIO_WritePin(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState);
-        HAL_GPIO_WritePin(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_SET);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_RESET);
     }
     else 
     {
-        HAL_GPIO_WritePin(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState);
-        HAL_GPIO_WritePin(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_SET);
     }
 }
 
@@ -63,27 +79,27 @@ void upated_ramp_target(void)
 {
     if(ramped_target < final_target)
     {
-        ramped_target += ramped_step;
+        ramped_target += ramp_step;
         if(ramped_target > final_target) ramped_target = final_target;
     }
     else if (ramped_target > final_target) 
     {
-        ramped_target -= ramped_step;
+        ramped_target -= ramp_step;
         if(ramped_target < final_target) ramped_target = final_target;
     }
 
-    target_pos = ramped_target;
+    target_pos = (int32_t)ramped_target;
 }
 
 void set_home(void)
 {
-    __HAL_TIM_SET_COUNTER(__HANDLE__, __COUNTER__);
+    __HAL_TIM_SET_COUNTER(&htim2, 0);
 }
 
 void motor_move_to(float revolutions)
 {
     final_target = (revolutions * REV_PER_COUNT);
-    ramped_target = (float)__HAL_TIM_GET_COUNTER(__HANDLE__);
+    ramped_target = (float)__HAL_TIM_GET_COUNTER(&htim2);
     integral_pos = 0.0f;
     prev_pos_error = 0.0f;
 }
